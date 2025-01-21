568e50778e455343757711d593d9ba23823811ad
Mongdar <teasetx@gmail.com>
2025-01-21 17:22:35 +0200

    first commit

---
+++ navbar.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const navBar1 = document.querySelector(".nav-bar1");
+  const navBar2 = document.querySelector(".nav-bar2");
+  const about = document.querySelector("#about");
+
+  // Toggle nav-bar2 visibility based on scroll position
+  window.addEventListener('scroll', () => {
+      if (window.scrollY > about.offsetHeight - (navBar1.offsetHeight/2)) {
+          navBar1.classList.remove("visible");
+          navBar2.classList.add("visible");
+      } else {
+          navBar1.classList.add("visible");
+          navBar2.classList.remove("visible");
+      }
+  });
+
+  // Initialize Smooth Scroll
+  const scroll = new SmoothScroll('.nav-bar a[href*="#"]', {
+      speed: 800,
+      speedAsDuration: true,
+  });
+});
