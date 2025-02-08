cdd26824ad660c5862d245537aaa3d03c2f61eb2
Mongdar <teasetx@gmail.com>
2025-02-08 22:35:13 +0200

    fix navigation styles

--- navbar.js
+++ navbar.js
@@ -2,6 +2,8 @@ document.addEventListener("DOMContentLoaded", () => {
   const navBar1 = document.querySelector(".nav-bar1");
   const navBar2 = document.querySelector(".nav-bar2");
   const home = document.querySelector("#home");
+  const accommmodation = document.querySelector("#accommmodation");
+  const santa = document.querySelector("#santas-house");
 
   // Toggle nav-bar2 visibility based on scroll position
   window.addEventListener('scroll', () => {
@@ -11,6 +13,18 @@ document.addEventListener("DOMContentLoaded", () => {
       } else {
           navBar1.classList.add("visible");
           navBar2.classList.remove("visible");
+      }
+
+      const isAfterAbout = window.scrollY > (accommmodation.getBoundingClientRect().top + window.scrollY - 20)
+      const isAfterAccomodation = window.scrollY > (santa.getBoundingClientRect().top + window.scrollY - 20)
+      if (isAfterAbout && !isAfterAccomodation) {
+        //   change navbar color and border color styles
+        navBar2.style.color = "black";
+        navBar2.style.borderColor = "black";
+
+      } else {
+        navBar2.style.color = "white";
+        navBar2.style.borderColor = "white";
       }
   });
 
