0271a35fb0ea46c62cd8730df57cee9dd8f6baa5
Mongdar <teasetx@gmail.com>
2025-02-09 23:11:19 +0200

    minor navbar styling fixes

--- navbar.js
+++ navbar.js
@@ -7,52 +7,59 @@ document.addEventListener("DOMContentLoaded", () => {
   const santa = document.querySelector("#santas-house");
   const weddings = document.querySelector("#weddings");
 
+  const navBarFunctionion = () => {
+    if (window.scrollY > home.offsetHeight - (navBar1.offsetHeight/2)) {
+      navBar1.classList.remove("visible");
+      navBar2.classList.add("visible");
+    } else {
+        navBar1.classList.add("visible");
+        navBar2.classList.remove("visible");
+    }
+
+    const isAbout = window.scrollY > (about.getBoundingClientRect().top + window.scrollY - 20)
+    const isAccommodation = window.scrollY > (accommmodation.getBoundingClientRect().top + window.scrollY - 20)
+    const isSanta = window.scrollY > (santa.getBoundingClientRect().top + window.scrollY - 20)
+    const isWeddings = window.scrollY > (weddings.getBoundingClientRect().top + window.scrollY - 20)
+
+    if (isAccommodation && !isSanta) {
+      //   change navbar color and border color styles
+      navBar2.style.color = "black";
+      navBar2.style.borderColor = "black";
+      navBar2.querySelector("a[href='#accommmodation']").classList.add("active");
+    } else {
+      navBar2.style.color = "white";
+      navBar2.style.borderColor = "white";
+      navBar2.querySelector("a[href='#accommmodation']").classList.remove("active");
+    }
+
+    
+    if (isAbout && !isAccommodation) {
+        navBar2.querySelector("a[href='#about']").classList.add("active");
+    } else {
+      navBar2.querySelector("a[href='#about']").classList.remove("active");
+    }
+
+    if (isSanta && !isWeddings) {
+      navBar2.querySelector("a[href='#santas-house']").classList.add("active");
+    } else {
+      navBar2.querySelector("a[href='#santas-house']").classList.remove("active");
+    }
+
+    
+    if (isWeddings) {
+      navBar2.querySelector("a[href='#weddings']").classList.add("active");
+    } else {
+      navBar2.querySelector("a[href='#weddings']").classList.remove("active");
+    }   
+  }
+
   // Toggle nav-bar2 visibility based on scroll position
   window.addEventListener('scroll', () => {
-      if (window.scrollY > home.offsetHeight - (navBar1.offsetHeight/2)) {
-          navBar1.classList.remove("visible");
-          navBar2.classList.add("visible");
-      } else {
-          navBar1.classList.add("visible");
-          navBar2.classList.remove("visible");
-      }
-
-      const isAbout = window.scrollY > (about.getBoundingClientRect().top + window.scrollY - 20)
-      const isAccommodation = window.scrollY > (accommmodation.getBoundingClientRect().top + window.scrollY - 20)
-      const isSanta = window.scrollY > (santa.getBoundingClientRect().top + window.scrollY - 20)
-      const isWeddings = window.scrollY > (weddings.getBoundingClientRect().top + window.scrollY - 20)
-
-      if (isAccommodation && !isSanta) {
-        //   change navbar color and border color styles
-        navBar2.style.color = "black";
-        navBar2.style.borderColor = "black";
-        navBar2.querySelector("a[href='#accommmodation']").classList.add("active");
-      } else {
-        navBar2.style.color = "white";
-        navBar2.style.borderColor = "white";
-        navBar2.querySelector("a[href='#accommmodation']").classList.remove("active");
-      }
-
-      
-      if (isAbout && !isAccommodation) {
-          navBar2.querySelector("a[href='#about']").classList.add("active");
-      } else {
-        navBar2.querySelector("a[href='#about']").classList.remove("active");
-      }
-
-      if (isSanta && !isWeddings) {
-        navBar2.querySelector("a[href='#santas-house']").classList.add("active");
-      } else {
-        navBar2.querySelector("a[href='#santas-house']").classList.remove("active");
-      }
-
-      
-      if (isWeddings) {
-        navBar2.querySelector("a[href='#weddings']").classList.add("active");
-      } else {
-        navBar2.querySelector("a[href='#weddings']").classList.remove("active");
-      }
+      navBarFunctionion();
   });
+  window.addEventListener('load', () => {
+    navBarFunctionion();
+});
 
   // Initialize Smooth Scroll
   const scroll = new SmoothScroll('.nav-bar a[href*="#"]', {
